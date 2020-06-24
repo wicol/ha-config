402e4c2d1e4378d53955857b093aec13c62ff273
wicol <wicol@users.noreply.github.com>
2020-06-24 21:57:18 +0200

    updates to groups

--- get_tags.sh
+++ get_tags.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -s https://registry.hub.docker.com/v1/repositories/homeassistant/home-assistant/tags | grep -oP "\d+\.\d+(\.\d+)?" | sort -n | tail -10
+curl -s https://registry.hub.docker.com/v1/repositories/homeassistant/home-assistant/tags | jq -r ".[].name" | grep "[0-9]" | sort -V | grep -v dev | tail -10
