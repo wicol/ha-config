099ae514469f99441e463947eab9b71e28df6523
wicol <wicol@users.noreply.github.com>
2020-12-13 14:41:55 +0100

    More state mapping

--- python_scripts/state_mapper.py
+++ python_scripts/state_mapper.py
@@ -21,8 +21,15 @@ if entity_id not in state_map:
 entity_state_map = {**defaults, **state_map[entity_id]}
 service_type = 'match_service' if entity_state == entity_state_map['state'] else 'nomatch_service'
 domain, service = entity_state_map[service_type].split('.')
+target = entity_state_map['target']
 
 logger.debug('%s, %s, %s, service_type: %s', entity_id, entity_state, entity_state_map, service_type)
-logger.info('%s -> %s. %s.%s -> %s', entity_id, entity_state, domain, service, entity_state_map['target'])
+logger.info('%s -> %s. %s.%s -> %s', entity_id, entity_state, domain, service, target)
 
-hass.services.call(domain, service, {'entity_id': entity_state_map['target']})
+# this is a way of determining if something is a list in this super weird scope
+if target.append:
+    for t in target:
+        hass.services.call(domain, service, {'entity_id': t})
+else:
+    hass.services.call(domain, service, {'entity_id': target})
+
